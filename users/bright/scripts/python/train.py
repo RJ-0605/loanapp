1b5cca5bdf6a501f4abcce154be7254b6d0be86d
EC2 Default User <ec2-user@ip-172-16-146-143.ec2.internal>
2023-07-17 18:00:50 +0000

    Finilizing

--- users/bright/scripts/python/train.py
+++
@@ -1,8 +0,0 @@
-#Import Libries
-from sklearn.ensemble import RandomForestClassifier
-from sagemaker import get_execution_role
-import argparser
-import sagemaker
-
-#Initial argpar
-print("HelloW World!")
